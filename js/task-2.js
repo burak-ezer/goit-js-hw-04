7ce004cbf706dc57874f8587fdfd4dab527b4994
burak-ezer <burakezerrr@gmail.com>
2025-05-20 02:50:58 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,10 @@
 function calcAverageCalories(days) {
   let total = 0;
   for (let calorie of Object.values(days)) {
-    console.log(calorie);
+    total += calorie.calories;
   }
+  let end = total/7;
+  
+  console.log(end);
+  return end;
 }
