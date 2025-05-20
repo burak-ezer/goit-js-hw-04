4faa6c26552b6feb3a6a96453ef60dad25490826
burak-ezer <burakezerrr@gmail.com>
2025-05-20 17:02:34 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,8 @@
 function isEnoughCapacity(products, containerSize) {
   let total = 0;
-  for (let product of Object.values(products)) {
+  for (const product of Object.values(products)) {
     total += product;
   }
-
-  if (total <= containerSize) {
-    
-    console.log(true);
-    return true;
-  } else {
-    
-    console.log(false);
-    return false;
-  }
+  total<=containerSize ? console.log(true) : console.log(false);
+  return total<=containerSize;
 }
